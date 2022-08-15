2ebaec1d545887e703954af717e62ebcd5bd5072
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-15 13:44:37 +0300

    Написание кода Server.java

---
+++ src/main/java/Server.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello! Server started!");
+
+        int port = 2125;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // ожидание подтверждения
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                System.out.println("New connection");
+                final String name = in.readLine();
+                out.println(String.format("Привет %s, твой порт подключения: [%d ]", name, clientSocket.getPort()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
